a0c6b4ad82ee1ddb6c9515ba52b1031fb3f4db68
servomouse <servomouse@protonmail.com>
2025-12-28 17:08:49 -0600

    Implementing core functionality

--- core.c
+++ core.c
@@ -1,2 +1,15 @@
 #include "core.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+text_buffer_t *root_buf;
+text_buffer_t *curr_buf;
+uint32_t curr_id;
+
+void set_current_buf(uint32_t id);
+text_buffer_t get_current_buf(uint32_t id);
+void create_buf(char *name);
+int is_buf_updated(uint32_t id);
+void save_buf(uint32_t id);
+void close_buf(uint32_t id);
